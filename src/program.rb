d27c248789032f4e3b70ba4a5f16e5dc1821d879
Daniel W. Amerson <dwamerson@gmail.com>
2018-12-19 20:09:51 -0500

    Eliminate global variable.

--- src/program.rb
+++ src/program.rb
@@ -8,20 +8,20 @@ end
 
 ARGV.clear # clear the command line args so that calls to gets aren't impacted
 
-$repository = AttributeRepository.new()
-
 # TODO error checking
-def parse_input_as_fact(input)
+def parse_input_as_fact(input, attribute_repository)
     are_related = input.index('/').nil?
     components = input.split(if are_related then '>' else '/' end)
     predicates_for_component_1 = components[0].split(':')
     predicates_for_component_2 = components[1].split(':')
 
-    $repository.add(predicates_for_component_1[0], predicates_for_component_1[1])
-    $repository.add(predicates_for_component_2[0], predicates_for_component_2[1])
+    attribute_repository.add(predicates_for_component_1[0], predicates_for_component_1[1])
+    attribute_repository.add(predicates_for_component_2[0], predicates_for_component_2[1])
 
     Fact.new(are_related, predicates_for_component_1[0], predicates_for_component_1[1], predicates_for_component_2[0], predicates_for_component_2[1])
 end
+
+repository = AttributeRepository.new()
 
 # Data isn't used to generate boards, but rather used to determine when inferences can be made 
 print "Enter the number of attribute types: "
@@ -34,7 +34,7 @@ all_facts_entered = false
 loop do
     print "Enter a fact in the form attribute_1:value_1>attribute_2:value_2 or attribute_1:value_1/attribute_2:value_2 to indicate whether or not a relationship exists between values: "
 
-    generated_fact = parse_input_as_fact(gets.chomp)
+    generated_fact = parse_input_as_fact(gets.chomp, repository)
     puts generated_fact
 
     if debug
